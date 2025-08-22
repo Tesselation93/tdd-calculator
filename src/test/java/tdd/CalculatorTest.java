56cf527b3afc89a34abf7f62e72085b91ebbdb4b
Tesselation93 <tessvanslyckspam@gmail.com>
2025-08-22 13:47:26 +0200

    CalculatorTest class added

---
+++ src/test/java/tdd/CalculatorTest.java
@@ -0,0 +1,37 @@
+package tdd;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void additionTwoNumbers() {
+        Calculator calc = new Calculator();
+        int result = calc.add(2, 3);
+        assertEquals(5, result);
+    }
+
+
+    @Test
+    void subtractionTwoNumbers() {
+        Calculator calc = new Calculator();
+        int result = calc.subtract(10, 5);
+        assertEquals(5, result);
+    }
+
+    @Test
+    void multiplicationTwoNumbers(){
+        Calculator calc = new Calculator();
+        int result = calc.multiply(10,10);
+        assertEquals(100, result);
+    }
+
+    @Test
+    void divisionTwoNumbers(){
+        Calculator calc = new Calculator ();
+        int result = calc.divide(25, 5);
+        assertEquals(5, result);
+    }
+
+}
