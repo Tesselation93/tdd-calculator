7b322957ce26acd499b854677cdf52cd3a21ab70
Tesselation93 <tessvanslyckspam@gmail.com>
2025-08-24 16:50:08 +0200

    Created branch featureAddString and made changes to the add method by overloading it to take a String input

--- src/main/java/tdd/Calculator.java
+++ src/main/java/tdd/Calculator.java
@@ -5,6 +5,12 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+    public int add(String numbers){
+        if (numbers.equals("")){
+            return 0;
+        }
+        return Integer.parseInt(numbers);
+    }
     public int subtract (int a, int b){
         return a - b;
     }
